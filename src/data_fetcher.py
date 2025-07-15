98eeb8485ffebb78b0357bccb9d2f1e020b42663
ashutosh0702 <baruah0702@gmail.com>
2025-07-16 01:39:59 +0530

    Debugging data_fetcher.py , gdalbuildvrt error

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -9,20 +9,31 @@ from shapely.geometry import box
 
 class DataFetcher:
     def __init__(self, bbox):
-        self.north, self.south, self.east, self.west = bbox
+        self.south, self.north, self.west, self.east = bbox
 
-    def fetch_osm_features(self):
+
+    def fetch_osm_features(self, tags=None):
+        
+        print("Fetching OSM features for:", self.south, self.north, self.west, self.east)
+
+        if tags is None:
+            tags = {
+                'building': True,
+                'highway': True,
+                'natural': True,
+                'landuse': True,
+                'waterway': True
+            }
         bbox_polygon = box(self.west, self.south, self.east, self.north)
-        gdf = ox.features_from_polygon(bbox_polygon, tags={
-            'building': True,
-            'highway': True,
-            'natural': True,
-            'landuse': True,
-            'waterway': True
-        })
+        gdf = ox.features_from_polygon(bbox_polygon, tags=tags)
         return gdf
+
     
     def fetch_dem(self, output_path='data/dem/clip.tif'):
+        print("Fetching DEM features for:", self.south, self.north, self.west, self.east)
+
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        elevation.clip(bounds=(self.west, self.south, self.east, self.north), output=output_path)
+        if not os.path.exists(output_path):
+            elevation.clip(bounds=(self.west, self.south, self.east, self.north), output=output_path)
         return output_path
+
